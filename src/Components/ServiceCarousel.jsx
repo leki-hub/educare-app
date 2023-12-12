6bae78efb199cef4b1593f3ef766708848e84874
Leki <lekisintole82@gmail.com>
2023-12-12 11:31:42 +0300

    css responsive

--- src/Components/ServiceCarousel.jsx
+++ src/Components/ServiceCarousel.jsx
@@ -6,13 +6,28 @@ import 'slick-carousel/slick/slick-theme.css';
 
 const ServiceCarousel = () => {
   const settings = {
-    dots: true,
+    // dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-  };
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };;
 
   return (
     <section className="services">
