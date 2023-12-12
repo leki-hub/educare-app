4d17d4888694ce4df66bbf3213b0900c567264bd
Leki <lekisintole82@gmail.com>
2023-12-12 12:08:50 +0300

    services component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from "react-router-dom"
+import ServiceCarousel from '../Components/ServiceCarousel'
 
 const Home = () => {
   return (
@@ -83,70 +84,8 @@ const Home = () => {
           </div>
       </div>
   </section>
-  {/* <!-- ***** Main Banner Area End ***** --> */}
-
-  <section className="services">
-  <div className="container">
-  <div className="row">
-    <div className="col-lg-12">
-      <div className="owl-service-item owl-carousel">
-      
-        <div className="item">
-          <div className="icon">
-            <img src="assets/images/service-icon-01.png" alt="" />
-          </div>
-          <div className="down-content">
-            <h4>Best Education</h4>
-            <p>Suspendisse tempor mauris a sem elementum bibendum. Praesent facilisis massa non vestibulum.</p>
-          </div>
-        </div>
-        
-        <div className="item">
-          <div className="icon">
-            <img src="assets/images/service-icon-02.png" alt="" />
-          </div>
-          <div className="down-content">
-            <h4>Best Teachers</h4>
-            <p>Suspendisse tempor mauris a sem elementum bibendum. Praesent facilisis massa non vestibulum.</p>
-          </div>
-        </div>
-        
-        <div className="item">
-          <div className="icon">
-            <img src="assets/images/service-icon-03.png" alt="" />
-          </div>
-          <div className="down-content">
-            <h4>Best Students</h4>
-            <p>Suspendisse tempor mauris a sem elementum bibendum. Praesent facilisis massa non vestibulum.</p>
-          </div>
-        </div>
-        
-        <div className="item">
-          <div className="icon">
-            <img src="assets/images/service-icon-02.png" alt="" />
-          </div>
-          <div className="down-content">
-            <h4>Online Meeting</h4>
-            <p>Suspendisse tempor mauris a sem elementum bibendum. Praesent facilisis massa non vestibulum.</p>
-          </div>
-        </div>
-        
-        <div className="item">
-          <div className="icon">
-            <img src="assets/images/service-icon-03.png" alt="" />
-          </div>
-          <div className="down-content">
-            <h4>Best Networking</h4>
-            <p>Suspendisse tempor mauris a sem elementum bibendum. Praesent facilisis massa non vestibulum.</p>
-          </div>
-        </div>
-        
-      </div>
-    </div>
-  </div>
-</div>
-
-  </section>
+  <ServiceCarousel />
+  
 
   <section className="upcoming-meetings" id="meetings">
     <div className="container">
