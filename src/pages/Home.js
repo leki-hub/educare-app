becaaa6a67eb3904d8284e1a0170b42f862ea85f
Leki <lekisintole82@gmail.com>
2023-12-15 22:21:51 +0300

    component added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -87,8 +87,7 @@ const Home = () => {
       </div>
   </section>
   <ServiceCarousel />
-  
-
+   
   <section className="upcoming-meetings" id="meetings">
     <div className="container">
       <div className="row">
@@ -213,70 +212,7 @@ const Home = () => {
             </div>
           </div>
         </div>
-        {/* <div className="col-lg-6">
-          <div className="accordions is-first-expanded">
-            <article className="accordion">
-                <div className="accordion-head">
-                    <span>About Edu Meeting HTML Template</span>
-                    <span className="icon">
-                        <i className="icon fa fa-chevron-right"></i>
-                    </span>
-                </div>
-                <div className="accordion-body">
-                    <div className="content">
-                        <p>If you want to get the latest collection of HTML CSS templates for your websites, you may visit <a rel="nofollow" href="https://www.toocss.com/" target="_blank">Too CSS website</a>. If you need a working contact form script, please visit <a href="https://templatemo.com/contact" target="_parent">our contact page</a> for more info.</p>
-                    </div>
-                </div>
-            </article>
-            <article className="accordion">
-                <div className="accordion-head">
-                    <span>HTML CSS Bootstrap Layout</span>
-                    <span className="icon">
-                        <i className="icon fa fa-chevron-right"></i>
-                    </span>
-                </div>
-                <div className="accordion-body">
-                    <div className="content">
-                        <p>Etiam posuere metus orci, vel consectetur elit imperdiet eu. Cras ipsum magna, maximus at semper sit amet, eleifend eget neque. Nunc facilisis quam purus, sed vulputate augue interdum vitae. Aliquam a elit massa.<br /><br />
-                        Nulla malesuada elit lacus, ac ultricies massa varius sed. Etiam eu metus eget nibh consequat aliquet. Proin fringilla, quam at euismod porttitor, odio odio tempus ligula, ut feugiat ex erat nec mauris. Donec viverra velit eget lectus sollicitudin tincidunt.</p>
-                    </div>
-                </div>
-            </article>
-            <article className="accordion">
-                <div className="accordion-head">
-                    <span>Please tell your friends</span>
-                    <span className="icon">
-                        <i className="icon fa fa-chevron-right"></i>
-                    </span>
-                </div>
-                <div className="accordion-body">
-                    <div className="content">
-                        <p>Ut vehicula mauris est, sed sodales justo rhoncus eu. Morbi porttitor quam velit, at ullamcorper justo suscipit sit amet. Quisque at suscipit mi, non efficitur velit.<br/><br/>
-                        Cras et tortor semper, placerat eros sit amet, porta est. Mauris porttitor sapien et quam volutpat luctus. Nullam sodales ipsum ac neque ultricies varius.</p>
-                    </div>
-                </div>
-            </article>
-            <article className="accordion last-accordion">
-                <div className="accordion-head">
-                    <span>Share this to your colleagues</span>
-                    <span className="icon">
-                        <i className="icon fa fa-chevron-right"></i>
-                    </span>
-                </div>
-                <div className="accordion-body">
-                    <div className="content">
-                        <p>Maecenas suscipit enim libero, vel lobortis justo condimentum id. Interdum et malesuada fames ac ante ipsum primis in faucibus.<br/><br/>
-                        Sed eleifend metus sit amet magna tristique, posuere laoreet arcu semper. Nulla pellentesque ut tortor sit amet maximus. In eu libero ullamcorper, semper nisi quis, convallis nisi.</p>
-                    </div>
-                </div>
-            </article>
-        </div>
-        </div> */}
       <HomeAccordion />
-
-
-
-
       </div>
     </div>
   </section>
