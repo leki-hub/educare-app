280ab210290fa6b75f4207c32113d9509bb9e5d7
Leki <lekisintole82@gmail.com>
2023-12-17 15:30:37 +0300

    corousel styling

--- src/Components/CourseCarousel.js
+++ src/Components/CourseCarousel.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-// import './CourseCarousel.css'; // Import a CSS file for custom styling
+
 
 const CourseCarousel = () => {
   const settings = {
@@ -27,6 +27,7 @@ const CourseCarousel = () => {
         },
       },
     ],
+    
   };
 
   return (
