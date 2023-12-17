bcd4989161d1cb9b55e29f5ecbdd0af9446e0618
Leki <lekisintole82@gmail.com>
2023-12-17 15:35:45 +0300

    count up animation

---
+++ src/Components/Countup.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import CountUp from 'react-countup';
+
+const CountUpAnimation = () => {
+  return (
+    <div className="col-lg-6">
+      <div className="row">
+        <div className="col-lg-12">
+          <h2>A Few Facts About Our University</h2>
+        </div>
+        <div className="col-lg-6">
+          <div className="row">
+            <div className="col-12">
+              <div className="count-area-content percentage">
+                <CountUp start={0} end={94} duration={2.5} separator="," className='text-warning' />
+                <div className="count-title">Succeeded Students</div>
+              </div>
+            </div>
+            <div className="col-12">
+              <div className="count-area-content">
+                <CountUp start={0} end={126} duration={3} separator="," className='text-warning' />
+                <div className="count-title">Current Teachers</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-6">
+          <div className="row">
+            <div className="col-12">
+              <div className="count-area-content new-students">
+                <CountUp start={0} end={2345} duration={4} separator="," className='text-warning' />
+                <div className="count-title">New Students</div>
+              </div>
+            </div>
+            <div className="col-12">
+              <div className="count-area-content">
+                <CountUp start={0} end={32} duration={2} separator="," className='text-warning' />
+                <div className="count-title">Awards</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CountUpAnimation;
