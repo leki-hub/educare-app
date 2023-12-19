c96e2b75e96583d0c8202cdabf8ffe4ec9e24b3d
Leki <lekisintole82@gmail.com>
2023-12-19 08:49:20 +0300

    Header no DRY

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../Components/HeaderTest.css';
+import '../Components/Header.css';
 import { useEffect } from 'react';
 
 
