fdad9c29873470c5bcd7129b7e099130977319ce
Leki <lekisintole82@gmail.com>
2023-12-22 10:51:59 +0300

    meeting component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Meetings from "./pages/MeetingsTest";
+import Meetings from "./pages/Meetings";
 import Header from "./Components/Header";
 import MeetingDetails from "./pages/MeetingDetails";
 
