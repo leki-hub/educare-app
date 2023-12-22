78fa562dc7cef7abcecbd917d75e7075d9612c30
Leki <lekisintole82@gmail.com>
2023-12-22 10:40:45 +0300

    filter meetings

--- src/pages/Meetings.js
+++ src/pages/Meetings.js
@@ -1,10 +1,52 @@
-import React from 'react'
+import React , { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 
 const Meetings = () => {
 
+  const [filter, setFilter] = useState('all'); // Initial filter state
 
+  const handleFilterClick = (filterValue) => {
+    setFilter(filterValue); // Update filter state based on clicked item
+  };
+
+// data representing meetings
+  const itemsData = [
+    { category: 'soon', title: 'New Kbisa Lecturers Meeting', date: 'Nov 12', price: '$14.00' },
+    { category: 'all', title: 'New  Kabisa Lecturers Meeting', date: 'Nov 12', price: '$14.00' },
+
+    { category: 'all', title: 'Online Kbisa Teaching Techniques', date: 'Nov 14', price: '$22.00' },
+    { category: 'imp', title: 'Online Teaching Techniques', date: 'Nov 14', price: '$22.00' },
+
+    { category: 'all', title: 'Network Teaching Concept', date: 'Nov 16', price: '$24.00' },
+    { category: 'soon', title: 'Network Teaching Concept', date: 'Nov 16', price: '$24.00' },
+
+    { category: 'all', title: 'Online Teaching Tools', date: 'Nov 18', price: '$32.00' },
+    { category: 'att', title: 'Online Teaching Tools', date: 'Nov 18', price: '$32.00' },
+
+    { category: 'all', title: 'New Lecturers Meeting', date: 'Nov 22', price: '$34.00' },
+    { category: 'att', title: 'New Lecturers Meeting', date: 'Nov 22', price: '$34.00' },
+
+    { category: 'all', title: 'New Teaching Techniques', date: 'Nov 22', price: '$45.00' },
+    { category: 'soon', title: 'New Teaching Techniques', date: 'Nov 22', price: '$45.00' },
+
+    { category: 'all', title: 'Technology Conference', date: 'Nov 24', price: '$52.00' },
+    { category: 'imp', title: 'Technology Conference', date: 'Nov 24', price: '$52.00' },
+
+    { category: 'all', title: 'Online Teaching Techniques', date: 'Nov 27', price: '$34.00' },
+    { category: 'imp', title: 'Online Teaching Techniques', date: 'Nov 27', price: '$34.00' },
+    { category: 'att', title: 'Online Teaching Techniques', date: 'Nov 27', price: '$34.00' },
+
+    { category: 'all', title: 'Online Social Networking', date: 'Nov 30', price: '$74.00' },
+    { category: 'soon', title: 'Online Social Networking', date: 'Nov 30', price: '$74.00' },
+    { category: 'att', title: 'Online Social Networking', date: 'Nov 30', price: '$74.00' },
+
+    { category: 'all', title: 'Instant Lecture Design', date: 'Nov 28', price: '$64.00' },
+    { category: 'soon', title: 'Instant Lecture Design', date: 'Nov 28', price: '$64.00' },
+    { category: 'imp', title: 'Instant Lecture Design', date: 'Nov 28', price: '$64.00' },
+
+
+  ];
 
 
   return (
@@ -25,173 +67,54 @@ const Meetings = () => {
       <div className="row">
         <div className="col-lg-12">
           <div className="row">
-            <div className="col-lg-12">
-              <div className="filters">
-                <ul>
-                  <li data-filter="*"  className="active">All Meetings</li>
-                  <li data-filter=".soon">Soon</li>
-                  <li data-filter=".imp">Important</li>
-                  <li data-filter=".att">Attractive</li>
-                </ul>
-              </div>
-            </div>
-            <div className="col-lg-12">
-              <div className="row grid">
-                <div className="col-lg-4 templatemo-item-col all soon">
-                  <div className="meeting-item">
-                    <div className="thumb">
-                      <div className="price">
-                        <span>$14.00</span>
-                      </div>
-                      <Link to ="/meeting-details"><img src="assets/images/meeting-01.jpg" alt="" /></Link>
+
+          <div className="col-lg-12">
+       <div className="filters">
+      <ul>
+        <li onClick={() => handleFilterClick('all')}>All</li>
+        <li onClick={() => handleFilterClick('soon')}>Soon</li>
+        <li onClick={() => handleFilterClick('imp')}>Important</li>
+        <li onClick={() => handleFilterClick('att')}>Attractive</li>
+      </ul>
+      </div>
+
+  //     {/* Render items based on the selected filter */}
+      <div className="row grid">
+        {itemsData.map((item, index) => {
+          const { category, title, date, price } = item; // Assuming your meeting object has these properties
+
+          // Check if the item matches the selected filter or if 'all' is selected
+          if (filter === category) {
+            return (
+              <div className={`col-lg-4 templatemo-item-col all ${category}`} key={index}>
+                <div className="meeting-item">
+                  <div className="thumb">
+                    <div className="price">
+                      <span>{price}</span>
                     </div>
-                    <div className="down-content">
-                      <div className="date">
-                        <h6>Nov <span>12</span></h6>
-                      </div>
-                      <Link to="/meeting-details"><h4>New Lecturers Meeting</h4></Link>
-                      <p>Morbi in libero blandit lectus<br />cursus ullamcorper.</p>
-                    </div>
+                   <Link to ="/meeting-details">  <img src={`assets/images/meeting-${index + 1}.jpg`} alt="" /> </Link>
                   </div>
-                </div>
-                <div className="col-lg-4 templatemo-item-col all imp">
-                  <div className="meeting-item">
-                    <div className="thumb">
-                      <div className="price">
-                        <span>$22.00</span>
-                      </div>
-                      <Link  to ="/meeting-details"><img src="assets/images/meeting-02.jpg" alt="" /></Link>
+                  <div className="down-content">
+                    <div className="date">
+                      <h6>Nov <span>{date}</span></h6>
                     </div>
-                    <div className="down-content">
-                      <div className="date">
-                        <h6>Nov <span>14</span></h6>
-                      </div>
-                      <Link to ="/meeting-details"><h4>Online Teaching Techniques</h4></Link>
-                      <p>Morbi in libero blandit lectus<br />cursus ullamcorper.</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-4 templatemo-item-col all soon">
-                  <div className="meeting-item">
-                    <div className="thumb">
-                      <div className="price">
-                        <span>$24.00</span>
-                      </div>
-                      <Link to ="/meeting-details"><img src="assets/images/meeting-03.jpg" alt="" /></Link>
-                    </div>
-                    <div className="down-content">
-                      <div className="date">
-                        <h6>Nov <span>16</span></h6>
-                      </div>
-                      <Link to ="/meeting-details"><h4>Network Teaching Concept</h4></Link>
-                      <p>Morbi in libero blandit lectus<br />cursus ullamcorper.</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-4 templatemo-item-col all att">
-                  <div className="meeting-item">
-                    <div className="thumb">
-                      <div className="price">
-                        <span>$32.00</span>
-                      </div>
-                      <Link to ="/meeting-details"><img src="assets/images/meeting-04.jpg" alt="" /></Link>
-                    </div>
-                    <div className="down-content">
-                      <div className="date">
-                        <h6>Nov <span>18</span></h6>
-                      </div>
-                      <Link to ="/meeting-details"><h4>Online Teaching Tools</h4></Link>
-                      <p>Morbi in libero blandit lectus<br />cursus ullamcorper.</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-4 templatemo-item-col all att">
-                  <div className="meeting-item">
-                    <div className="thumb">
-                      <div className="price">
-                        <span>$34.00</span>
-                      </div>
-                      <Link to ="/meeting-details"><img src="assets/images/meeting-02.jpg" alt="" /></Link>
-                    </div>
-                    <div className="down-content">
-                      <div className="date">
-                        <h6>Nov <span>22</span></h6>
-                      </div>
-                      <Link to ="/meeting-details"><h4>New Teaching Techniques</h4></Link>
-                      <p>Morbi in libero blandit lectus<br />cursus ullamcorper.</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-4 templatemo-item-col all imp">
-                  <div className="meeting-item">
-                    <div className="thumb">
-                      <div className="price">
-                        <span>$45.00</span>
-                      </div>
-                      <Link to ="/meeting-details"><img src="assets/images/meeting-03.jpg" alt="" /></Link>
-                    </div>
-                    <div className="down-content">
-                      <div className="date">
-                        <h6>Nov <span>24</span></h6>
-                      </div>
-                      <Link to ="/meeting-details"><h4>Technology Conference</h4></Link>
-                      <p>TemplateMo is the best website<br />when it comes to Free CSS.</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-4 templatemo-item-col all imp att">
-                  <div className="meeting-item">
-                    <div className="thumb">
-                      <div className="price">
-                        <span>$52.00</span>
-                      </div>
-                      <Link to ="/meeting-details"><img src="assets/images/meeting-01.jpg" alt="" /></Link>
-                    </div>
-                    <div className="down-content">
-                      <div className="date">
-                        <h6>Nov <span>27</span></h6>
-                      </div>
-                      <Link to ="/meeting-details"><h4>Online Teaching Techniques</h4></Link>
-                      <p>Morbi in libero blandit lectus<br />cursus ullamcorper.</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-4 templatemo-item-col all soon imp">
-                  <div className="meeting-item">
-                    <div className="thumb">
-                      <div className="price">
-                        <span>$64.00</span>
-                      </div>
-                      <Link to ="/meeting-details"><img src="assets/images/meeting-02.jpg" alt="" /></Link>
-                    </div>
-                    <div className="down-content">
-                      <div className="date">
-                        <h6>Nov <span>28</span></h6>
-                      </div>
-                      <Link to ="/meeting-details"><h4>Instant Lecture Design</h4></Link>
-                      <p>Morbi in libero blandit lectus<br />cursus ullamcorper.</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-4 templatemo-item-col all att soon">
-                  <div className="meeting-item">
-                    <div className="thumb">
-                      <div className="price">
-                        <span>$74.00</span>
-                      </div>
-                      <Link to ="/meeting-details"><img src="assets/images/meeting-03.jpg" alt="" /></Link>
-                    </div>
-                    <div className="down-content">
-                      <div className="date">
-                        <h6>Nov <span>30</span></h6>
-                      </div>
-                      <Link to ="/meeting-details"><h4>Online Social Networking</h4></Link>
-                      <p>Morbi in libero blandit lectus<br />cursus ullamcorper.</p>
-                    </div>
+                    <h4>{title}</h4>
+                    <p>Mambo  in libero blandit lectus cursus ullamcorper.</p>
                   </div>
                 </div>
               </div>
-            </div>
+        
+        );
+          }
+
+   
+     return null; // If the item doesn't match the filter, don't render it
+        })}
+      </div>
+    </div>
+
+
+            
             <div className="col-lg-12">
               <div className="pagination">
                 <ul>
