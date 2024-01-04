6b6f579115037502a9d8ced57bbff0b51b3e689d
Leki <lekisintole82@gmail.com>
2024-01-04 10:33:22 +0300

    href tags

--- src/pages/MeetingDetails.jsx
+++ src/pages/MeetingDetails.jsx
@@ -29,13 +29,13 @@ const MeetingDetails = () => {
                   <div className="date">
                     <h6>Nov <span>12</span></h6>
                   </div>
-                  <Link to="/meetings"><img src="assets/images/single-meeting.jpg" alt="meeting image" /></Link>
+                  <Link to="/meetings"><img src="assets/images/single-meeting.jpg" alt="meeting" /></Link>
                 </div>
                 <div className="down-content">
-                  <a href="meeting-details.html"><h4>Online Teaching and Learning Tools</h4></a>
+                  <a href="/"><h4>Online Teaching and Learning Tools</h4></a>
                   <p>Recreio dos Bandeirantes, Rio de Janeiro - RJ, 22795-008, Brazil</p>
                   <p className="description">
-                    This is an edu meeting HTML CSS template provided by <a href="https://templatemo.com/" target="_blank" rel="nofollow">TemplateMo website</a>. This is a Bootstrap v5.1.3 layout. If you need more free website templates like this one, please visit our website TemplateMo. Please tell your friends about our website. Thank you. If you want to get the latest collection of HTML CSS templates for your websites, you may visit <a rel="nofollow" href="https://www.toocss.com/" target="_blank">Too CSS website</a>. If you need a working contact form script, please visit <a href="https://templatemo.com/contact" target="_parent">our contact page</a> for more info.
+                    This is an edu meeting HTML CSS template provided by <a href="/"  >Emmanuel</a>. This is a Bootstrap v5.1.3 layout. If you need more free website templates like this one, please visit our website TemplateMo. Please tell your friends about our website. Thank you. If you want to get the latest collection of HTML CSS templates for your websites, you may visit <a rel="noreferrer" href="https://www.toocss.com/" target="_blank">Too CSS website</a>. If you need a working contact form script, please visit <a href="https://templatemo.com/contact" target="_parent">our contact page</a> for more info.
                     
                     <br/>You are allowed to use this edu meeting CSS template for your school or university or business. You can feel free to modify or edit this layout. You are not allowed to redistribute the template ZIP file on any other template website. Please contact us for more information.
                   </p>
